f03847302b8ed9e2b53a14a8b761b787004b545d
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2021-08-13 20:58:33 +0900

    change : About

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    <Layout pageTitle="About">
+      <StaticImage alt="the working cat" src="../images/shigotoneko.jpeg" />
+      <div />
+      <br />
+      <div>おれです、自己を表すSNSアカウントがありません</div>
+      <div>自作のアイコンが欲しいです</div>
     </Layout>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
