744cb3750b1e3097e8cff3584bd5dff598d91de1
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2021-08-15 23:17:25 +0900

    add static ogp

--- src/pages/{mdx.slug}.js
+++ src/pages/{mdx.slug}.js
@@ -3,15 +3,28 @@ import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
+import { Helmet } from "react-helmet";
 
 const BlogPost = ({ data }) => {
   const image = getImage(data.mdx.frontmatter.hero_image);
+  const title = data.mdx.frontmatter.title;
   return (
-    <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>Posted: {data.mdx.frontmatter.date}</p>
-      <GatsbyImage image={image} />
-      <MDXRenderer>{data.mdx.body}</MDXRenderer>
-    </Layout>
+    <>
+      <Helmet>
+        <html lang="jp" />
+        <title>{title}</title>
+        <meta property="og:site_name" content={title} />
+        <meta property="og:image" content={image} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:image" content={image} />
+      </Helmet>
+      <Layout pageTitle={title}>
+        <p>Posted: {data.mdx.frontmatter.date}</p>
+        <GatsbyImage image={image} />
+        <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      </Layout>
+    </>
   );
 };
 
