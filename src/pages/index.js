683d2f2ebdb0958755b2157ffdd58fe3425c7cb3
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2021-08-16 01:30:54 +0900

    indexjsにも書いてみた

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,31 @@
 import * as React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
+import { Helmet } from "react-helmet";
 
 const BlogPage = ({ data }) => {
   return (
-    <Layout pageTitle="kizi ichiran">
-      {data.allMdx.nodes.map((node) => (
-        <article key={node.id}>
-          <h2>
-            <Link to={`/${node.slug}`}>{node.frontmatter.title}</Link>
-          </h2>
-          <p>Posted: {node.frontmatter.date}</p>
-        </article>
-      ))}
-    </Layout>
+    <>
+      <Helmet>
+        <meta property="og:site_name" content="murakami blog" />
+        <meta name="description" content="村上のブログ" />
+        <meta property="og:image" content="../images/shigotoneko.jpeg" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="murakami blog" />
+        <meta name="twitter:description" content="村上のブログ" />
+        <meta name="twitter:image" content="../images/shigotoneko.jpeg" />
+      </Helmet>
+      <Layout pageTitle="kizi ichiran">
+        {data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/${node.slug}`}>{node.frontmatter.title}</Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+        ))}
+      </Layout>
+    </>
   );
 };
 
