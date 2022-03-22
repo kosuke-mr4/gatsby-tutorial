3cdc6a4374931d66182dbbefcc295dc42d2e2ea7
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2022-03-23 02:28:41 +0900

    add : disp percent

--- src/components/piChart.js
+++ src/components/piChart.js
@@ -2,20 +2,40 @@ import React from "react";
 
 import { VictoryPie, VictoryTooltip, VictoryLabel } from "victory";
 
-const validAp = "#388087";
-const validA = "#6fb3b8";
-const validB = "#badfe7";
-const validC = "#ffff00";
-const validD = "#8b4513";
+// const validAp = "#388087";
+// const validA = "#6fb3b8";
+// const validB = "#badfe7";
+// const validC = "#ffff00";
+// const validD = "#8b4513";
 
-const validGraphicColor = [validAp, validA, validB, validC, validD];
+// const validGraphicColor = [validAp, validA, validB, validC, validD];
+
+const CalcPer = (data) => {
+  if (data.text == "D") {
+    return "D credit is Out of Calc :(";
+  } else {
+    let totalValidCredit = 0;
+    data.data.forEach((element) => {
+      if (element["x"] !== "D") {
+        totalValidCredit += parseFloat(element["y"]);
+      }
+    });
+
+    const thisCredit = parseFloat(data.data[data.index]["y"]);
+    const creditPer = (thisCredit / totalValidCredit) * 100;
+
+    return creditPer.toString().substring(0, 5);
+  }
+};
 
 const CustomLabel = (prop) => {
+  console.log(prop);
   return (
     <g>
       <VictoryLabel {...prop} />
       <VictoryTooltip
         {...prop}
+        text={prop.text + "\n" + CalcPer(prop)}
         x={200}
         y={250}
         orientation="top"
@@ -23,7 +43,7 @@ const CustomLabel = (prop) => {
         cornerRadius={50}
         flyoutWidth={100}
         flyoutHeight={100}
-        flyoutStyle={{ fill: "#0000" }}
+        flyoutStyle={{ stroke: "none", fill: "#0000" }}
       />
     </g>
   );
