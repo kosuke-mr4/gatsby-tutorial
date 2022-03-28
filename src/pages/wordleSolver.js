9c0d39fe8587b98dd1ead15a613bc969f842f13a
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2022-03-29 02:08:42 +0900

    add : files // ブランチ変更のため退避

---
+++ src/pages/wordleSolver.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const WordleSolver = () => {
+  return (
+    <>
+      <Layout pageTitle={"Wordle Solver"}></Layout>
+    </>
+  );
+};
+
+export default WordleSolver;
