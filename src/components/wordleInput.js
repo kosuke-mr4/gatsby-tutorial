2c97db63c3e7b94a4f10397af8f414c633996669
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2022-03-30 23:35:33 +0900

    add : input text

--- src/components/wordleInput.js
+++ src/components/wordleInput.js
@@ -1,6 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const WordleInput = () => {};
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.1em;
+`;
+const Label = styled.label`
+  padding: 0.1em;
+  cursor: pointer;
+`;
+const Input = styled.input.attrs({
+  type: "text",
+  maxLength: "1",
+})`
+  border: 5px solid #000;
+  border-radius: 2px;
+  color: #000000;
+  font-size: 4rem;
+  width: 4.1rem;
+  margin: 0px;
+  text-align: center;
+  border-radius: 10px;
+`;
+
+const ColoredBox = styled.div`
+  width: 4rem;
+  height: 4rem;
+  background: green;
+  border-radius: 1rem;
+  margin-right: 0.5rem;
+`;
+
+const WordleInput = () => {
+  const [greenState, setGreenState] = useState([]);
+  const [yellowState, setYellowState] = useState([]);
+  const [grayState, setGrayState] = useState([]);
+
+  return (
+    <>
+      <LetterArray />
+      <LetterArray />
+      <LetterArray />
+    </>
+  );
+};
+
+const LetterArray = () => {
+  return (
+    <div>
+      <Wrapper>
+        <ColoredBox />
+        {[...Array(5)].map((_, index) => (
+          <div key={index}>
+            <Label>
+              <Input></Input>
+            </Label>
+          </div>
+        ))}
+      </Wrapper>
+    </div>
+  );
+};
 
 export default WordleInput;
--- src/pages/wordleSolver.js
+++ src/pages/wordleSolver.js
@@ -1,10 +1,13 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import WordleInput from "../components/wordleInput.js";
 
 const WordleSolver = () => {
   return (
     <>
-      <Layout pageTitle={"Wordle Solver"}></Layout>
+      <Layout pageTitle={"Wordle Solver"}>
+        <WordleInput />
+      </Layout>
     </>
   );
 };
