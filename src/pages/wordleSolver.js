097d73f960c54f8304071ef050435fad12686e44
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2022-04-05 00:22:04 +0900

    add : nossr

--- src/pages/wordleSolver.js
+++ src/pages/wordleSolver.js
@@ -1,13 +1,16 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import WordleInput from "../components/wordleInput.js";
+import NoSSR from "react-no-ssr";
 
 const WordleSolver = () => {
   return (
     <>
-      <Layout pageTitle={"Wordle Solver"}>
-        <WordleInput />
-      </Layout>
+      <NoSSR>
+        <Layout pageTitle={"Wordle Solver"}>
+          <WordleInput />
+        </Layout>
+      </NoSSR>
     </>
   );
 };
