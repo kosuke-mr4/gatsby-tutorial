ceb07e663f93767a97f327d11c7a7c5430fcc2f6
kosuke-mr4 <s1911381@coins.tsukuba.ac.jp>
2022-05-03 09:36:06 +0900

    add : state

--- src/pages/oneHour.js
+++ src/pages/oneHour.js
@@ -1,29 +1,73 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/layout";
 import dayjs from "dayjs";
+import styled from "styled-components";
+
+const LoggerButton = styled.button`
+  text-align: center;
+`;
 
 const OneHourPage = () => {
   const [savedDate, setSavedDate] = useState("");
   const [currentDate, setCurrentDate] = useState("");
-  // ここで格納した場合１分ごとの操作とかはどうしようかな
+
+  const [passedMinute, setPassedMinute] = useState(0);
 
   useEffect(() => {
     let localDate = localStorage.getItem("date");
+    const startDate = dayjs().format(); // 現在時刻
 
     if (localDate == null) {
-      const nowDate = dayjs().format();
-      localStorage.setItem("date", nowDate);
-      console.log("set!!!");
+      localStorage.setItem("date", startDate);
+      setSavedDate(startDate);
+      setCurrentDate(startDate);
     } else {
       console.log(localDate);
+      setSavedDate(localDate);
+      setCurrentDate(startDate);
+      SetDiff(savedDate, currentDate, setPassedMinute);
     }
   });
 
+  // 60秒ごとにcurrentDateをc更新
+  // まず円を描画する
+  // 円の角度をstate参照でできるようにする
+
   return (
     <>
-      <Layout pageTitle={"one hour timer"}>AAAAAAAAAAAAAAAAAAAAAAAA</Layout>
+      <Layout pageTitle={"one hour timer"}>
+        <LoggerButton
+          onClick={() => SetDiff(savedDate, currentDate, setPassedMinute)}
+        >
+          logger
+        </LoggerButton>
+
+        <LoggerButton
+          onClick={() =>
+            console.log(
+              "savedDate : ",
+              savedDate,
+              "\ncurrentDate : ",
+              currentDate,
+              "\n sub : ",
+              passedMinute
+            )
+          }
+        >
+          state logger
+        </LoggerButton>
+      </Layout>
     </>
   );
+};
+
+const SetDiff = (savedDate, currentDate, setPassedMinute) => {
+  const savedDateObj = dayjs(savedDate);
+  const currentDateObj = dayjs(currentDate);
+
+  const passed = currentDateObj.diff(savedDateObj, "minute");
+  console.log(passed);
+  setPassedMinute(passed);
 };
 
 export default OneHourPage;
